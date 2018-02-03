9fe3007aeeca60b1c7fa2a08a71a2a288f50753f
cwy007 <chanweiyan007@gmail.com>
2018-02-03 23:40:22 +0800

    g resource user email password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203152450) do
+ActiveRecord::Schema.define(version: 20180203153525) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.string "author_name"
     t.datetime "published_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
