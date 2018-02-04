16485a9a4c105e6a715ee4c7b6ac2f3f5adad5e1
cwy007 <chanweiyan007@gmail.com>
2018-02-04 14:29:54 +0800

    adding a "Remember Me" checkbox

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
-
+  has_secure_password  # 会限制密码不得为空
   validates_uniqueness_of :email
+  before_create { generate_token(:auth_token) }
+  
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203153525) do
+ActiveRecord::Schema.define(version: 20180204051353) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180203153525) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
   end
 
 end
