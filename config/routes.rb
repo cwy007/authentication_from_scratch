c609bba2fd383cbcca9ec46e5ec801a0f6471dfc
cwy007 <chanweiyan007@gmail.com>
2018-02-04 16:32:54 +0800

    implement welcome page & _nav.html.erb partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'welcome', to: 'welcome#index',    as: 'welcome'
   get '/signup', to: 'users#new',        as: 'signup'
   get '/login',  to: 'sessions#new',     as: 'login'
   get '/logout', to: 'sessions#destroy', as: 'logout'
@@ -8,6 +9,6 @@ Rails.application.routes.draw do
   resources :sessions
   resources :password_resets
   resources :articles
-  
-  root 'articles#index'
+
+  root 'welcome#index'
 end
